5977b21335b4860d3950ea1f4db6622743edd598
Kody Samaroo <kody.samaroo@gmail.com>
2021-09-13 14:46:11 -0400

    functioing login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import { Switch, Route } from "react-router-dom"
-
 import NavBar from "./NavBar"
 import Main from "./Main"
 import Collection from "./Collection"
+import Login from "./Login"
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
@@ -27,23 +27,31 @@ function App() {
   return (
   <div>
     <NavBar 
-    toggleDark={toggleDark}
-    darkMode={darkMode}
-       />
+      toggleDark={toggleDark}
+      darkMode={darkMode}
+    />
+
     <Switch>
+    
       <Route exact path="/">
         <Main
-        darkMode={darkMode}
-        selected={selected}
-        setSelected={setSelected}
+          darkMode={darkMode}
+          selected={selected}
+          setSelected={setSelected}
         />
       </Route>
+
       <Route exact path="/collection">
         <Collection
-        selectProject={selectProject} 
-        darkMode={darkMode}
+          selectProject={selectProject} 
+          darkMode={darkMode}
         />
       </Route>
+
+      <Route exact path="/login">
+        <Login />
+      </Route>
+
     </Switch>
   </div>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,34 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
-import { GiFoundryBucket } from "react-icons/gi"
+import React from "react";
+import '../styles/navbar.css'
+import { NavLink } from "react-router-dom";
+import { GiFoundryBucket } from "react-icons/gi";
 
-function NavBar({ toggleDark, darkMode}){
+function NavBar({ toggleDark, darkMode }){
 
     return (
         <div className="nav">
 
-                <span className="logo-icon"  style={darkMode ? {color: "white"} : {color: "black"}}>   
-                    <GiFoundryBucket />
-                </span>
+            <span className="logo-icon"  style={darkMode ? {color: "white"} : {color: "black"}}>
+                <GiFoundryBucket />
+            </span>
 
-                <h1 className="logo-name" style={darkMode ? {color: "white"} : {color: "black"}}>Code Bucket</h1>
-
-            <button className="dark-mode" onClick={toggleDark}>
-            Dark Mode
-            </button>
+            <h1 className="logo-name" style={darkMode ? {color: "white"} : {color: "black"}}>Code Bucket</h1>
 
             <NavLink exact to="/" className="home-button">
                 Home
             </NavLink>
 
-            <NavLink exact to="/collection" className="project-collection">
+            <NavLink exact to="/collection" className="collection-button">
                 Collection
             </NavLink>
+
+            <NavLink exact to="/login" className="login-button">
+                Login/Signup
+            </NavLink>
+
+            <button className="dark-mode" onClick={toggleDark}>
+                Dark Mode
+            </button>
 
         </div>
     )
