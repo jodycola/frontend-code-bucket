ae1d977cd63f119052f95927ced4607fc02bdabb
Kody Samaroo <kody.samaroo@gmail.com>
2021-09-18 16:18:09 -0400

    set up login handlers awaiting CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/login.css'
 
 function Login() {
+    const [login, setLogin] = useState({
+        email: "",
+        password: "",
+        name: "",
+        verify: ""
+    })
+
+    // LOGIN HANDLER
+    function handleLogin(){
+        console.log(login)
+    }
+
+    // SIGNUP HANDLER
+    function handleRegister(){
+        
+    }
 
     return (
-        <div>
+        <div className="login">
+
             <h1> Login </h1>
+
+            <form onSubmit={handleLogin}>
+                <label className=""> Email Address </label>
+                <input 
+                    placeholder="Enter email address"
+                    className="input"
+                    type="text"
+                    name="email"
+                    value={login.email}
+                    onChange={(e) => setLogin({
+                        ...login, [e.target.name]: e.target.value})}
+                />
+
+                <label> Password </label>
+                <input 
+                    placeholder="Enter password"
+                    className="input"
+                    type="password"
+                    name="password"
+                    value={login.password}
+                    onChange={(e) => setLogin({
+                        ...login, [e.target.name]: e.target.value})}
+                />
+                <button
+                    className="button"
+                    type="submit"
+                > Log in </button>
+            </form>
         </div>
     )
 }
